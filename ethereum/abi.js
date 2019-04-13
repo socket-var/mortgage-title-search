28ca202e64f098c2b695d815dc22ece608f4d4c0
saketh varma <sakethbobby@gmail.com>
2019-04-13 00:18:51 -0400

    Add new abi

--- ethereum/abi.js
+++ ethereum/abi.js
@@ -1,122 +1,31 @@
 module.exports = [
-    {
-      "inputs": [],
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "constructor",
-      "signature": "constructor"
-    },
-    {
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "fallback"
-    },
-    {
-      "constant": true,
-      "inputs": [
-        {
-          "name": "userAddress",
-          "type": "address"
-        }
-      ],
-      "name": "balanceOf",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function",
-      "signature": "0x70a08231"
-    },
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "register",
-      "outputs": [],
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "function",
-      "signature": "0x1aa3a008"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "userAddress",
-          "type": "address"
-        }
-      ],
-      "name": "unregister",
-      "outputs": [],
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "function",
-      "signature": "0x2ec2c246"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "toAirline",
-          "type": "address"
-        }
-      ],
-      "name": "settlePayment",
-      "outputs": [],
-      "payable": true,
-      "stateMutability": "payable",
-      "type": "function",
-      "signature": "0x2109dc88"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "toAirline",
-          "type": "address"
-        },
-        {
-          "name": "orderHash",
-          "type": "uint256"
-        }
-      ],
-      "name": "request",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function",
-      "signature": "0xc8c01a55"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "fromAirline",
-          "type": "address"
-        },
-        {
-          "name": "orderHash",
-          "type": "uint256"
-        }
-      ],
-      "name": "response",
-      "outputs": [
-        {
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function",
-      "signature": "0x3ef9e3e4"
-    }
-  ]
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "text",
+        type: "bytes32"
+      }
+    ],
+    name: "AddedRecord",
+    type: "event",
+    signature:
+      "0x7dfc57a9525538776c977cb1e9851c5be5b9e0a6bafd9fbf3ca94ee6d4dd42e2"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "bar",
+        type: "bytes32"
+      }
+    ],
+    name: "addRecord",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+    signature: "0x571a1d73"
+  }
+];
